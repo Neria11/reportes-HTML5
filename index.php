9081f64ff011481c72b8e46d586f572065b7a6fa
iostream23 <iostream23cout@gmail.com>
2015-04-10 11:24:41 -0500

    section nuevo
    se ha agregado un section nuevo

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
 	<input type="button" value="Entrar">
 	</form>
 	</center>
+<section class="contener">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</section>
 
 <footer>
 	<p><center>POWER BY HTML5</center></p>	
